b415a5410d3be49d6a9f34d2e829529c4689736b
kraiem56 <bkmohamed016@gmail.com>
2022-09-11 21:30:02 +0200

    third

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="home__meta">
         <h1 className="home__text pz__10">WELCOME TO MY WORLD</h1>
         <h2 className="home__text pz__10">Hi, I’m Mohamed Ben Kraiem</h2>
-        <h3 className="home__text sweet pz__10">Full-Stack JS Developer.</h3>
+        <h3 className="home__text sweet pz__10">Full-Stack JavaScript Developer.</h3>
         <h4 className="home__text pz__10">based in Tunisia.</h4>
       </div>
     </div>
